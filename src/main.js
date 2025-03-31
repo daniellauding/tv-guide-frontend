426b7b69aac0cf9bda227fb75001424311cb2462
daniellauding <daniel@lauding.se>
2025-03-31 22:57:57 +0200

    new mobile nav

--- src/main.js
+++ src/main.js
@@ -926,24 +926,28 @@ function showProgramModal(channelId, programTime) {
             `
                 : ''
             }
-            <div class="modal__hero" style="background-color: ${channel.color || '#1a1a1a'}">
+            <div class="modal__hero">
+                <div class="modal__hero-header">
+                  ${
+                    !isMobile
+                      ? `
+                      <button onclick="closeModal()" class="modal__close-btn">✕</button>
+                      <button onclick="shareProgram('${channel.id}', '${program.time}')" class="modal__share-btn">
+                          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z"/>
+                          </svg>
+                      </button>
+                  `
+                      : ''
+                  }
+                </div>
                 <div class="modal__hero-content">
-                    ${
-                      !isMobile
-                        ? `
-                        <button onclick="closeModal()" class="modal__close-btn">✕</button>
-                        <button onclick="shareProgram('${channel.id}', '${program.time}')" class="modal__share-btn">
-                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z"/>
-                            </svg>
-                        </button>
-                    `
-                        : ''
-                    }
                     <div class="modal__channel-info">
-                        <img src="${channel.logo}" alt="${
-    channel.name
-  }" class="modal__channel-logo">
+                        <div class="modal__channel-logo">
+                          <img src="${channel.logo}" alt="${channel.name}" 
+                          class="program-card__logo-img"
+                            onerror="this.parentElement.innerHTML = '${channel.name[0]}'">
+                        </div>
                         <h1 class="modal__program-title">${program.title}</h1>
                     </div>
                     <div class="modal__meta">
